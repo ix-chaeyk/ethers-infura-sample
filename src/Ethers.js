1c08b149747006373b101dceea4207d2d259369f
chaeyk <chaeyk@neowiz.com>
2022-11-08 11:57:43 +0900

    add web3 test

--- src/Ethers.js
+++ src/Ethers.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import { ethers } from "ethers";
+import Web3 from "web3";
 
 class Ethers extends Component {
   constructor(props) {
@@ -14,7 +15,7 @@ class Ethers extends Component {
       url: "https://goerli.infura.io/v3/449391f15f3444d08ea51a4d62141418",
       password: "1839fa09992948dd9e8084a387ab5729",
       // headers: {
-      //   authorization: 'Basic :1839fa09992948dd9e8084a387ab5729'
+      //   authorization: 'Basic ' + btoa(':1839fa09992948dd9e8084a387ab5729')
       // }
     });
     const blockNumber = await provider.getBlockNumber();
@@ -30,11 +31,18 @@ class Ethers extends Component {
     this.setState({ blockNumber });
   }
 
+  async testWeb3() {
+    const web3 = new Web3('https://:1839fa09992948dd9e8084a387ab5729@goerli.infura.io/v3/449391f15f3444d08ea51a4d62141418');
+    const blockNumber = await web3.eth.getBlockNumber();
+    this.setState({ blockNumber });
+  }
+
   render() {
     return (
       <div>
         <button onClick={() => this.testJsonRpcProvider()}>Test JsonRpcProvider</button><br />
         <button onClick={() => this.testInfuraProvider()}>Test InfuraProvider</button><br />
+        <button onClick={() => this.testWeb3()}>Test Web3</button><br />
         <p>{this.state?.blockNumber ?? ""}</p>
       </div>
     );
